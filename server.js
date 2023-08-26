be613b5514b22fb2cf16399de6afb07403fb7c23
tatukare <tatukare@gmail.com>
2023-08-26 17:30:55 -0500

    Post users and install body-parcer

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 const PORT = 3000;
 
 let counter = 0;
@@ -9,6 +10,9 @@ const usersList = [
   { name: 'Rigo', email: 'rigonator@gmail.com' },
   { name: 'Egan', email: 'eganzipa@gmail.com' },
 ];
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/api/users/:id', (req, res) => {
   const { id } = req.params;
@@ -24,6 +28,21 @@ app.get('/api/users', (req, res) => {
     return res.status(404).json({ message: 'Not Users Found' });
 
   res.json({ data: { ...usersList } });
+});
+
+app.post('/api/users', (req, res) => {
+  const { name, email } = req.body;
+
+  if (!name || !email)
+    return res.status(400).json({ message: 'User and email are required' });
+
+  const user = usersList.filter((u) => u.email === email);
+  console.log(user);
+  if (user.length > 0)
+    return res.status(409).json({ message: 'User already exists' });
+
+  usersList.push({ name, email });
+  res.status(201).json({ name, email });
 });
 
 app.use('/', (req, res) => {
