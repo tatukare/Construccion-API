eb7e63a6de92986d3dc38ab4a049a737a130a585
tatukare <tatukare@gmail.com>
2023-08-26 16:48:03 -0500

    Implemented get usersp

--- server.js
+++ server.js
@@ -4,14 +4,33 @@ const PORT = 3000;
 
 let counter = 0;
 
-app.get('/api/users/:id', (req, res) => {});
-app.get('/api/users', (req, res) => {});
+const usersList = [
+  { name: 'Carlos', email: 'example@gmail.com' },
+  { name: 'Rigo', email: 'rigonator@gmail.com' },
+  { name: 'Egan', email: 'eganzipa@gmail.com' },
+];
+
+app.get('/api/users/:id', (req, res) => {
+  const { id } = req.params;
+  const user = usersList[`${id}`];
+
+  if (!user) return res.status(404).json({ message: 'User not Found' });
+
+  res.json({ data: user });
+});
+
+app.get('/api/users', (req, res) => {
+  if (usersList.length === 0)
+    return res.status(404).json({ message: 'Not Users Found' });
+
+  res.json({ data: { ...usersList } });
+});
 
 app.use('/', (req, res) => {
   counter++;
   console.log('counter: ', counter);
   console.log('Hello World in terminal');
-  res.status(404).send({ message: 'Not found' });
+  res.status(404).json({ message: 'Not found' });
 });
 
 app.listen(PORT, () => {
